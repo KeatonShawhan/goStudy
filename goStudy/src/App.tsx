252a8d2dc8c9e6e8603831c3e2459cfa2273be46
Clayson Fellner <clay.fell8@gmail.com>
2023-08-21 16:43:05 -0700

    Created Login Card

--- goStudy/src/App.tsx
+++ goStudy/src/App.tsx
@@ -1,8 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import LoginCard from "./components/LoginCard";
 
 function App() {
 
   return (
-    <p>Hello</p>
+    <Box display='flex' justifyContent='center' alignItems='center' width='100%' height='30rem'>
+      <LoginCard/>
+    </Box>
   )
 }
 
---
+++ goStudy/src/components/LoginCard.tsx
@@ -0,0 +1,20 @@
+import { Box, FormControl, FormLabel, Input, Text, Link, Button } from '@chakra-ui/react'
+
+const LoginCard = () => {
+  return (
+    <Box width='30rem' height='25rem' background='gray.700' borderRadius='5px' padding='2rem'>
+    <Text color='#cfd9e8' fontWeight='bold' fontSize='2xl'>WELCOME BACK!</Text>
+    <Text fontSize='2xs' marginBottom='1rem'>Don't have an account? <Link color='#6896d9'>Sign up</Link></Text>
+    <FormControl marginBottom='1rem'>
+    <FormLabel color='#cfd9e8'>Email</FormLabel>
+    <Input type='email' marginBottom='1rem' border='1px solid #6896d9'/>
+    <FormLabel color='#cfd9e8'>Password</FormLabel>
+    <Input type='password' border='1px solid #6896d9' marginBottom='1rem'/>
+    <Link marginBottom='1.5rem' fontSize='2xs' display='flex' justifyContent='right' color='#6896d9'>Forgot Password?</Link>
+    <Button borderRadius='10px' width='100%' background='#6896d9'>Sign In</Button>
+    </FormControl>
+    </Box>
+  )
+}
+
+export default LoginCard
