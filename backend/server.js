d36be3c1103ba38a237c793a14d134486e0f381a
KeatonShawhan <64229449+KeatonShawhan@users.noreply.github.com>
2023-08-21 14:24:09 -0700

    adding server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
