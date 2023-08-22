0cae150a218e96258f40f846c0ef69931c310737
Clayson Fellner <clay.fell8@gmail.com>
2023-08-21 18:13:48 -0700

    Refactoring - Fixed react app.

---
+++ frontend/Frontend-React-App/src/App.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import LoginCard from "./components/LoginCard";
+
+function App() {
+
+  return (
+    <Box display='flex' justifyContent='center' alignItems='center' width='100%' height='30rem'>
+      <LoginCard/>
+    </Box>
+  )
+}
+
+export default App
