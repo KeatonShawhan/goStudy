caa44fa8499812e743ab8e03d49e72fda57a38fc
Asphodelaceae <95513065+Asphodelaceae@users.noreply.github.com>
2023-08-24 00:17:18 -0500

    added sudo to cmds

--- init_compose.sh
+++ init_compose.sh
@@ -12,7 +12,7 @@ docker-compose up -d
 
 sleep 10
 
-results = $(docker exec -i gostudy-db-1 mysql -u root -p $DATABASE_PASSWORD < verify.sql)
+results=$(sudo docker exec -i gostudy-db-1 mysql -u   -p $DATABASE_PASSWORD < verify.sql)
 
 # Parse the results
 # This is a basic check to see if any of the results are empty, indicating a missing item
@@ -20,7 +20,7 @@ if echo "$results" | grep -q "NULL"; then
     echo "Verification failed! Some items are missing. Re-running init.sql..."
     
     # Run the init.sql script to initialize the database again
-    docker exec -i gostudy-db-1 mysql -u root -p $DATABASE_PASSWORD < init.sql
+    sudo docker exec -i gostudy-db-1 mysql -u root -p $DATABASE_PASSWORD < init.sql
 
     echo "Initialization complete. You may want to verify again to ensure all items are present."
 else
