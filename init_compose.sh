3272296f3d43d21276d4d3ed92557e44c3f7d74a
Asphodelaceae <95513065+Asphodelaceae@users.noreply.github.com>
2023-08-24 00:12:05 -0500

    fixed small error in compose

--- init_compose.sh
+++ init_compose.sh
@@ -10,9 +10,9 @@ sed -i "s/{{PASSWORD}}/$DATABASE_PASSWORD/g" init.sql
 # Start the MySQL container (or any other commands you need)
 docker-compose up -d
 
-sleep 20
+sleep 10
 
-results = $(docker exec -i mysql-db-1 mysql -u root -p $DATABASE_PASSWORD < verify.sql)
+results = $(docker exec -i gostudy-db-1 mysql -u root -p $DATABASE_PASSWORD < verify.sql)
 
 # Parse the results
 # This is a basic check to see if any of the results are empty, indicating a missing item
@@ -20,7 +20,7 @@ if echo "$results" | grep -q "NULL"; then
     echo "Verification failed! Some items are missing. Re-running init.sql..."
     
     # Run the init.sql script to initialize the database again
-    docker exec -i mysql-db-1 mysql -u root -p $DATABASE_PASSWORD < init.sql
+    docker exec -i gostudy-db-1 mysql -u root -p $DATABASE_PASSWORD < init.sql
 
     echo "Initialization complete. You may want to verify again to ensure all items are present."
 else
