41f330e240024a573c5bb605f81aee4744a927fd
Clayson Fellner <clay.fell8@gmail.com>
2023-08-26 13:08:21 -0700

    Updated initial color mode.

--- frontend/Frontend-React-App/src/theme.ts
+++ frontend/Frontend-React-App/src/theme.ts
@@ -1,7 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 import '@fontsource/nunito'
 import '@fontsource/overpass'
+
 const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: "true"
+  },
     fonts: {
       heading: `'Overpass', sans-serif`,
       body: `'Nunito', sans-serif`,
