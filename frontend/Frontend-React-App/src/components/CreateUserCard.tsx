47310170cdaa886037c58e2a7ef25a740133417d
Clayson Fellner <clay.fell8@gmail.com>
2023-08-29 21:36:27 -0700

    Updated CreateUserCard and implemented an API post

--- frontend/Frontend-React-App/src/components/CreateUserCard.tsx
+++ frontend/Frontend-React-App/src/components/CreateUserCard.tsx
@@ -1,14 +1,45 @@
 import { FormControl, FormLabel, Input, Button, Box, Text } from "@chakra-ui/react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-/*interface User {
-  email: string;
-  username: string;
-  major: string;
-  password: string;
-}*/
+interface User {
+  username: string,
+  password: string,
+  email: string,
+  major: string
+}
+interface UserSuccess {
+  message: string;
+  user_id: number;
+}
+
+interface UserError {
+  error: string;
+}
+
+const dummyUser: User = {
+  username: 'clay',
+  password: 'bob',
+  email: 'clay.fell8@gmail.com',
+  major: 'comp sci'
+}
 
 
 const CreateUserCard = () => {
+
+  const [error, setError] = useState<UserError[]>([]);
+  const [users, setUsers] = useState<UserSuccess[]>([]);
+  const JSON_dummyUser = JSON.stringify(dummyUser);
+
+
+  useEffect(() => {
+    axios
+    .post('http://webapp:3001/register', JSON_dummyUser)
+    .then((res) => setUsers(res.data))
+    .catch(err => setError(err)) 
+  }, [])
+
+
   return (
     <Box width='100%' display='flex' justifyContent='center' alignItems='center' marginTop='1rem'>
       <Box  width='30rem' background='gray.700' borderRadius='5px' padding='2rem' marginBottom='1rem'>
@@ -32,8 +63,11 @@ const CreateUserCard = () => {
 
             <Button borderRadius='10px' width='100%' background='#6896d9'>Sign In</Button>
           </FormControl>  
+          <Text>{Array.isArray(users) ? users.map(user => <p id={user.user_id.toString()}>{user.message}</p>) : "user"}</Text>
+          <Text>{Array.isArray(error) ? error.map(error => <p id={error.error}>{error.error}</p>): "error"}</Text>
       </Box>
     </Box>
+
     
   )
 }
