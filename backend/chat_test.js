f3d76812ba3482722b9e4c54a674a70e8a9b88ea
KeatonShawhan <64229449+KeatonShawhan@users.noreply.github.com>
2023-08-29 18:47:04 -0700

    lol

--- backend/chat_test.js
+++ backend/chat_test.js
@@ -3,15 +3,14 @@ const io = require('socket.io-client');
 // Connect to server
 const socket = io('http://localhost:3001');
 
-// Handle connection
 socket.on('connect', () => {
   console.log('Connected to server.');
 
   // Add additional event handlers here. For example:
-  socket.emit('join', { groupId: 1, userId: 2 });  // Notice 'userId' instead of 'username'
-
+  socket.emit('join', { groupId: 1, userId: 2 });  // userId 2 is joining groupId 1
+  
   // To send a message:
-  socket.emit('sendMessage', 'Message 2', 2, 1);
+  socket.emit('sendMessage', 'Message 2!', 2, 1);  // send 'Message 2!' to groupId 1 from userId 2
 
   // To receive a message:
   socket.on('message', (message) => {
