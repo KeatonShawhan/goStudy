c7e2073d58b8f13095aa90cba7d4f4a16aca0350
KeatonShawhan <64229449+KeatonShawhan@users.noreply.github.com>
2023-08-29 17:55:11 -0700

    chat test script

---
+++ backend/chat_test.js
@@ -0,0 +1,25 @@
+const io = require('socket.io-client');
+
+// Connect to server
+const socket = io('http://localhost:3001');
+
+// Handle connection
+socket.on('connect', () => {
+  console.log('Connected to server.');
+
+  // Add additional event handlers here. For example:
+  socket.emit('join', { groupId: 1, username: 'TestUser' });
+  
+  // To send a message:
+  socket.emit('sendMessage', 'Hello, group!', 1);
+
+  // To receive a message:
+  socket.on('message', (message) => {
+    console.log('Received message:', message);
+  });
+});
+
+// Handle disconnection
+socket.on('disconnect', () => {
+  console.log('Disconnected from server.');
+});
