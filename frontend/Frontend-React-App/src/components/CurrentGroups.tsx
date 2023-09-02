70aa8a368c5e1b8645245da4ea3da049301f0808
Clayson Fellner <clay.fell8@gmail.com>
2023-09-01 21:29:33 -0700

    Updated Page

--- frontend/Frontend-React-App/src/components/CurrentGroups.tsx
+++ frontend/Frontend-React-App/src/components/CurrentGroups.tsx
@@ -1,19 +1,46 @@
 import { Box, List, ListItem, Text } from "@chakra-ui/react"
+import axios from "axios";
+import { useState } from "react";
+const hostName = import.meta.env.VITE_HOST_NAME;
+
+
 
 const CurrentGroups = () => {
+  const [studyGroups, setStudyGroups] = useState();
+  const [error, setError] = useState();
+  const createStudyGroupRequest = () => {
+      axios // make API Request                   
+      .get(`${hostName}/api/my-study-groups`, {
+          headers: {
+          'Content-Type': 'application/json',
+          // This thing below is the header for how you pass in the token to a protected endpoint
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+      })
+      .then((res) => {
+          setStudyGroups(res.data)
+      })
+      // do smth with error if it happens, look at the endpoint in routes.js for specific error code meanings or ask Luca
+      .catch(err => setError(err))
+  }
   return (
-    <Box width='13rem' position='fixed' right='0' height='100%' marginTop='5rem' background={"gray.900"}>
-      <Box display='flex' justifyContent='center' alignItems='center'>
-        <Text color='white' fontSize='3xl' paddingTop='2rem'>Current Groups</Text>
-        <List>
-            <ListItem color='white'>hello</ListItem>
-            <ListItem color='white'>hello</ListItem>
-            <ListItem color='white'>hello</ListItem>
-            <ListItem color='white'>hello</ListItem>
-            <ListItem color='white'>hello</ListItem>
-        </List>
-      </Box>   
+    <>
+    <Box width='80%' height='100%' position='fixed' left='0' marginTop='5rem'>
+
     </Box>
+    <Box width='20%' position='fixed' right='0' height='100%' marginTop='5rem' marginBottom='1.5rem'background={"gray.900"}>
+      <Box>
+        <Box display='flex' justifyContent='center' alignItems='center'>
+          <Text color='white' fontSize='2xl' paddingTop='.5rem' >Current Groups</Text>
+        </Box>
+        <Box display='flex' justifyContent='center' alignItems='center'>
+          <List>
+            <ListItem color='#6896d9'>hello</ListItem>
+          </List>
+        </Box>
+      </Box>
+    </Box>
+    </>
   )
 }
 
--- frontend/Frontend-React-App/src/components/NavBar.tsx
+++ frontend/Frontend-React-App/src/components/NavBar.tsx
@@ -1,11 +1,88 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Button, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+const hostName = import.meta.env.VITE_HOST_NAME;
+
+interface StudyGroup {
+  group_name: string;
+  subject: string;
+}
 
 const NavBar = () => {
+  const [studyGroup, setStudyGroup] = useState<StudyGroup>({ 
+    group_name: '',
+    subject: ''
+  }) 
+  const [error, setError] = useState({
+    error: ''
+  })
+  const [isVisible, setVisible] = useState(false);
+
+    const studyGroupRequest = () => {
+        axios // make API Request                   
+        .post(`${hostName}/api/create-study-group`, studyGroup, {
+            headers: {
+            'Content-Type': 'application/json',
+            // This thing below is the header for how you pass in the token to a protected endpoint
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then((res) => {
+            // successful request gives you this json obj { message: 'Study group created', group_id: groupId }
+            // do what you need to with that info, like redirect to that study group page
+            console.log(res)
+        })
+        // do smth with error if it happens, look at the endpoint in routes.js for specific error code meanings or ask Luca
+        .catch(err => setError(err))
+    }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setStudyGroup(() => ({
+      ...studyGroup,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    studyGroupRequest();
+    showForm();
+  }
+
+  const showForm = () => {
+    setVisible(!isVisible)
+  }
+
   return (
+    <>
     <Box padding='1rem' height='5rem' width='100%' background="gray.900" position='fixed' top='0 'display='flex' justifyContent="space-between">
-        <Text>GoStudy</Text>
+      <Box display='flex' alignItems='center'>
+      <Text fontSize='4xl'>GoStudy</Text>
+      </Box>
+      <Box display='flex' alignItems='center'>
+      <Text as='button' onClick={showForm}>Create Group</Text>
+      </Box>
         <Box width='50px' borderRadius='100%' background='#fff'/>
         </Box>
+        {isVisible ? <Box display='flex' justifyContent='center' alignItems='center'>
+          <Box marginTop='10rem' width='20rem' height='20rem' background='gray.900' padding='1rem'>
+       <form onSubmit={handleSubmit}>
+      <FormControl marginBottom='1rem'>
+
+        <FormLabel color='#cfd9e8' htmlFor='group_name'>Group Name</FormLabel>
+        <Input type='text' name='group_name' required id='group_name' marginBottom='1rem' border='1px solid #6896d9' onChange={handleInputChange}/>
+
+        <FormLabel color='#cfd9e8' htmlFor='subject'>Subject</FormLabel>
+        <Input type='text' id='subject' required name='subject' border='1px solid #6896d9' marginBottom='1rem' onChange={handleInputChange}/>
+
+        <Button borderRadius='10px' width='100%' background='#6896d9' type='submit' >Create Group</Button>
+      </FormControl>
+      <Text>{error.error}</Text>
+      </form>
+      </Box>
+    </Box> : null}
+    </>
   )
 }
 
