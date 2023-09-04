91e0b14ec28f6ab33f60b6ced6bd1f25f6910416
Clayson Fellner <clay.fell8@gmail.com>
2023-09-04 14:05:54 -0700

    Please change the color mode

--- frontend/Frontend-React-App/src/main.tsx
+++ frontend/Frontend-React-App/src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
 import  theme  from './theme.ts'
 
- 
+useEffect(() => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'light' ? 'dark' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+
+}, [])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
