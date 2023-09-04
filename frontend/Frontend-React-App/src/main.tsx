c8f4fac3120ecf25e395a0bb9e78c634e3d5a1eb
Clayson Fellner <clay.fell8@gmail.com>
2023-09-04 13:46:01 -0700

    Color mode hotfix

--- frontend/Frontend-React-App/src/main.tsx
+++ frontend/Frontend-React-App/src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 import './index.css'
 import  theme  from './theme.ts'
 
+document.documentElement.setAttribute('data-theme', 'dark');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
