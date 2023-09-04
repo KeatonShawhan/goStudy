b997fcc329595ad709dc7ef49d737b26f7c9aa3e
Clayson Fellner <clay.fell8@gmail.com>
2023-09-04 13:43:40 -0700

    I am losing it

--- frontend/Frontend-React-App/src/main.tsx
+++ frontend/Frontend-React-App/src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
 import  theme  from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
     <ChakraProvider theme={theme}>
     <App />
     </ChakraProvider>
