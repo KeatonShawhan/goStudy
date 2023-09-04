36de0d19579e0a74c2aed303a8ce07d7ca30ed28
Clayson Fellner <clay.fell8@gmail.com>
2023-09-04 14:09:53 -0700

    I clearly don't understand

--- frontend/Frontend-React-App/src/main.tsx
+++ frontend/Frontend-React-App/src/main.tsx
@@ -1,21 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
 import  theme  from './theme.ts'
 
-useEffect(() => {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'light' ? 'dark' : 'dark';
-  document.documentElement.setAttribute('data-theme', newTheme);
-
-}, [])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode="dark"/>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
--- frontend/Frontend-React-App/src/theme.ts
+++ frontend/Frontend-React-App/src/theme.ts
@@ -1,10 +1,14 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 import '@fontsource/nunito'
 import '@fontsource/overpass'
 
-
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false
+}
 
 const theme = extendTheme({
+    config,
     fonts: {
       heading: `'Overpass', sans-serif`,
       body: `'Nunito', sans-serif`,
