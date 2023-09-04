52da81449ad1f5035ce1b6ad10aeef22e31ce0d3
Clayson Fellner <clay.fell8@gmail.com>
2023-09-04 13:50:25 -0700

    i need this

--- frontend/Frontend-React-App/src/main.tsx
+++ frontend/Frontend-React-App/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
 import  theme  from './theme.ts'
 
-document.documentElement.setAttribute('data-theme', 'dark');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
     <App />
     </ChakraProvider>
   </React.StrictMode>,
